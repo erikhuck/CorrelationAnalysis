da18e72bba8670b8b3af07a832a21f58acbda49d
Erik Huckvale <erikhuck@gmail.com>
2021-01-15 13:53:47 -0700

    Removed pointless code from the debug data script

--- debug_data.py
+++ debug_data.py
@@ -2,21 +2,22 @@
 
 from sys import argv
 from pandas import DataFrame, read_csv
-from pickle import load, dump
+from pickle import load
 from random import seed, shuffle
+
+from utils import COL_TYPES_PICKLE_PATH
 
 
 def debug_data():
     """See module description"""
 
     data_path: str = argv[1]
-    col_types_path: str = argv[2]
-    n_cols: int = int(argv[3])
+    n_cols: int = int(argv[2])
 
     # Ensure the same columns are selected each time this script is run
     seed(0)
 
-    with open(col_types_path, 'rb') as f:
+    with open(COL_TYPES_PICKLE_PATH, 'rb') as f:
         col_types: dict = load(f)
 
     # Sort the headers for consistency
@@ -25,13 +26,6 @@ def debug_data():
     headers: list = headers[0:n_cols]
 
     # Make a subset of the data and column types
-    col_types: dict = {header: col_types[header] for header in headers}
-    assert set(headers) == set(col_types.keys())
-    assert len(headers) == len(col_types)
-
-    with open('data/debug-col-types.p', 'wb') as f:
-        dump(col_types, f)
-
     data: DataFrame = read_csv(data_path, usecols=['PTID'] + headers)
     data.to_csv('data/debug-data.csv', index=False)
 
