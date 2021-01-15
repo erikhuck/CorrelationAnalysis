ebeee3dc6bd4c6dae73e6c87016b4f4385d524ba
Erik Huckvale <erikhuck@gmail.com>
2021-01-14 20:17:09 -0700

    fixed stash conflicts

--- inter_counts_table.py
+++ inter_counts_table.py
@@ -35,84 +35,6 @@ def main():
 
     uncorrected_alpha, corrected_alpha = load(open(ALPHAS_PATH, 'rb'))
     col_types: dict = get_col_types()
-<<<<<<< Updated upstream
-
-    start_idx, stop_idx = iterate_comp_dicts(
-        comp_dict_dir=comp_dict_dir, idx=idx, section_size=section_size, func=count_comparisons, col_types=col_types,
-        table=table, super_alpha=super_alpha, corrected_alpha=corrected_alpha, uncorrected_alpha=uncorrected_alpha
-    )
-=======
-<<<<<<< Updated upstream
-    comp_dict_dir: str = join('data', comp_dict_dir)
-    comp_dicts: list = listdir(comp_dict_dir)
-
-    # Remove the files that aren't comparison dictionaries
-    new_comp_dicts: list = []
-
-    for comp_dict in comp_dicts:
-        if comp_dict.endswith('.p'):
-            new_comp_dicts.append(comp_dict)
-
-    comp_dicts: list = sorted(new_comp_dicts)
-    del new_comp_dicts
-
-    n_dicts: int = len(comp_dicts)
-    print('Total Number Of Comparison Dictionaries:', n_dicts)
-    start_idx: int = idx * section_size
-
-    if start_idx >= n_dicts:
-        print(
-            'ERROR: Start index of {} is greater than or equal to the number of comparison dictionaries {}'.format(
-                start_idx, n_dicts
-            )
-        )
-        exit(1)
-
-    print('Start Index:', start_idx)
-    stop_idx: int = min(start_idx + section_size, n_dicts)
-    print('Stop Index:', stop_idx)
-    comp_dicts: list = comp_dicts[start_idx:stop_idx]
-
-    for comp_dict in comp_dicts:
-        comp_dict: str = join(comp_dict_dir, comp_dict)
-        print('Loading Comparison Dictionary At:', comp_dict)
-        time1: float = time()
-        comp_dict: dict = load(open(comp_dict, 'rb'))
-        time2: float = time()
-        print('Load Time: {:.2f} minutes'.format((time2 - time1) / 60))
-
-        n_comparisons: int = len(comp_dict)
-        print('Total Number Of Comparisons:', n_comparisons)
-        time1: float = time()
-
-        for (feat1, feat2), p in tqdm(comp_dict.items()):
-            type1: str = get_type(header=feat1, col_types=col_types)
-            type2: str = get_type(header=feat2, col_types=col_types)
-
-            if type1 == NOMINAL_TYPE and type2 == NOMINAL_TYPE:
-                row_key: str = NOM_NOM_KEY
-            elif type1 == NUMERIC_TYPE and type2 == NUMERIC_TYPE:
-                row_key: str = NUM_NUM_KEY
-            else:
-                row_key: str = NUM_NOM_KEY
-
-            table[TOTAL_KEY][row_key] += 1
-
-            if p < super_alpha:
-                col_key: str = SUPER_ALPHA_KEY
-            elif p < corrected_alpha:
-                col_key: str = CORRECTED_ALPHA_KEY
-            elif p < uncorrected_alpha:
-                col_key: str = UNCORRECTED_ALPHA_KEY
-            else:
-                col_key: str = INSIGNIFICANT_KEY
-
-            table[col_key][row_key] += 1
-
-        time2: float = time()
-        print('Time Iterating Through Comparisons: {:.2f} minutes'.format((time2 - time1) / 60))
-=======
-
     start_idx, stop_idx = iterate_comp_dicts(
         comp_dict_dir=comp_dict_dir, idx=idx, section_size=section_size, func=count_comparisons, col_types=col_types,
         table=table, super_alpha=super_alpha, corrected_alpha=corrected_alpha, uncorrected_alpha=uncorrected_alpha,
@@ -129,20 +51,6 @@ def main():
     table.to_csv(counts_table_path)
 
 
-<<<<<<< Updated upstream
-def count_comparisons(
-    feat1: str, feat2: str, p: float, col_types: dict, table: DataFrame, super_alpha: float, corrected_alpha: float,
-    uncorrected_alpha: float
-):
-    """Determines the type of comparison and the alpha it is lower than and updates the counts table accordingly"""
-
-    row_key: str = get_comparison_type(feat1=feat1, feat2=feat2, col_types=col_types)
-    table[TOTAL_KEY][row_key] += 1
-
-    if p < super_alpha:
-=======
-<<<<<<< Updated upstream
-=======
 def make_table(table_type: str) -> DataFrame:
     """Creates the counts table of the given type"""
 
@@ -188,7 +96,6 @@ def count_comparisons(
     if p < MIN_ALPHA:
         col_key: str = MAX_SIGNIFICANCE_KEY
     elif p < super_alpha:
->>>>>>> Stashed changes
         col_key: str = SUPER_ALPHA_KEY
     elif p < corrected_alpha:
         col_key: str = CORRECTED_ALPHA_KEY
@@ -200,8 +107,6 @@ def count_comparisons(
     table[col_key][row_key] += 1
 
 
-<<<<<<< Updated upstream
-=======
 def get_comparison_domains(feat1: str, feat2: str, col_types: dict) -> str:
     """Indicates which domains the two features of a comparison come from"""
 
@@ -241,7 +146,5 @@ def get_domain(feat: str, col_types: dict) -> int:
     return EXPRESSION_KEY
 
 
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 if __name__ == '__main__':
     main()
